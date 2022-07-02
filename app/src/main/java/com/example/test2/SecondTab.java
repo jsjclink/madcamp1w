785ceff619e68a25bd4849511ffb9b42f9b0638c
2018008777 <2018008777@hanyang.ac.kr>
2022-07-02 18:54:19 +0900

    Floating Button Add

--- app/src/main/java/com/example/test2/SecondTab.java
+++ app/src/main/java/com/example/test2/SecondTab.java
@@ -3,7 +3,9 @@ package com.example.test2;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +16,61 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
 public class SecondTab extends Fragment {
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.sample_1, R.drawable.sample_2,
+            R.drawable.sample_3, R.drawable.sample_4,
+            R.drawable.sample_5, R.drawable.sample_6,
+            R.drawable.sample_7, R.drawable.sample_8,
+            R.drawable.sample_9, R.drawable.sample_10,
+            R.drawable.sample_11, R.drawable.sample_12,
+            R.drawable.sample_13, R.drawable.sample_14,
+            R.drawable.sample_15, R.drawable.sample_16,
+            R.drawable.sample_17, R.drawable.sample_18,
+            R.drawable.sample_19, R.drawable.sample_20
+    };
+    private ArrayList<Uri> pictures;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Picture Uri Array Initialization
+        pictures = new ArrayList<>();
+        for (Integer i : mThumbIds) {
+            pictures.add(Uri.parse("android.resource://com.example.test2/" + i));
+        }
+
         View v = inflater.inflate(R.layout.second_tab, container, false);
 
         GridView gridview = (GridView) v.findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(getActivity()));
+
+        // Setting floating action button to fetch image from gallery
+        ActivityResultLauncher<String> mGetContent =
+                registerForActivityResult(new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri uri) {
+                        pictures.add(uri);
+                        gridview.requestLayout();
+                    }
+                });
+        FloatingActionButton tab2AddPicture = v.findViewById(R.id.Tab2AddPictureFB);
+        tab2AddPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGetContent.launch("image/*");
+            }
+        });
 
         return v;
     }
@@ -40,7 +88,7 @@ public class SecondTab extends Fragment {
         }
 
         public int getCount() {
-            return mThumbIds.length;
+            return pictures.size();
         }
 
         public Object getItem(int position) {
@@ -64,7 +112,7 @@ public class SecondTab extends Fragment {
                 imageView = (ImageView) convertView;
             }
 
-            imageView.setImageResource(mThumbIds[position]);
+            imageView.setImageURI(pictures.get(position));
             imageView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -76,19 +124,5 @@ public class SecondTab extends Fragment {
             });
             return imageView;
         }
-
-        // references to our images
-        private Integer[] mThumbIds = {
-                R.drawable.sample_1, R.drawable.sample_2,
-                R.drawable.sample_3, R.drawable.sample_4,
-                R.drawable.sample_5, R.drawable.sample_6,
-                R.drawable.sample_7, R.drawable.sample_8,
-                R.drawable.sample_9, R.drawable.sample_10,
-                R.drawable.sample_11, R.drawable.sample_12,
-                R.drawable.sample_13, R.drawable.sample_14,
-                R.drawable.sample_15, R.drawable.sample_16,
-                R.drawable.sample_17, R.drawable.sample_18,
-                R.drawable.sample_19, R.drawable.sample_20
-        };
     }
 }
