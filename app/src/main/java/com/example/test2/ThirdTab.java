9738271be18b31c1725add32cbd197bac313da0e
2018008777 <2018008777@hanyang.ac.kr>
2022-07-05 13:56:08 +0900

    SecondTab is refreshed only when ThirdTab saves bitmap.

--- app/src/main/java/com/example/test2/ThirdTab.java
+++ app/src/main/java/com/example/test2/ThirdTab.java
@@ -13,7 +13,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
